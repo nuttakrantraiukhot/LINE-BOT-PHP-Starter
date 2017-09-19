373b6a24b834f63c4b7876d53744c7bebf5a89cd
nuttakrantraiukhot <31967424+nuttakrantraiukhot@users.noreply.github.com>
2017-09-19 20:42:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,84 @@
-<php?
 {
-  "replyToken": "/jlPwu9B9zHK3uZu+NlIRAoTqhYISpzevh1qeAVyqNKDVpM5X7+GtW3aK1gdB6k96Fkd14LElyqGZ2y1Ln59AY2/F6+0jVtmsIyUFArhRmB7ui1WcYNAEDNYwslg4UvDVxz/fhKLSxZFOmLah1gaTAdB04t89/1O/w1cDnyilFU=",
-  "type": "message",
-  "timestamp": 1462629479859,
-  "source": {
-    "type": "user",
-    "userId": "Uc86e42f9f17d087f2c6ca6fae436028c"
-  },
-  "message": {
-    "id": "325708",
-    "type": "text",
-    "text": "Hello, world"
-  }
+  "events": [
+      {
+        "replyToken": "/jlPwu9B9zHK3uZu+NlIRAoTqhYISpzevh1qeAVyqNKDVpM5X7+GtW3aK1gdB6k96Fkd14LElyqGZ2y1Ln59AY2/F6+0jVtmsIyUFArhRmB7ui1WcYNAEDNYwslg4UvDVxz/fhKLSxZFOmLah1gaTAdB04t89/1O/w1cDnyilFU=",
+        "type": "message",
+        "timestamp": 1462629479859,
+        "source": {
+             "type": "user",
+             "userId": "Uc86e42f9f17d087f2c6ca6fae436028c"
+         },
+         "message": {
+             "id": "325708",
+             "type": "text",
+             "text": "Hello, world"
+          }
+      }
+  ]
 }
-?>
+
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
+-d '{
+    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+    "messages":[
+        {
+            "type":"text",
+            "text":"Hello, user"
+        },
+        {
+            "type":"text",
+            "text":"May I help you?"
+        }
+    ]
+}' https://api.line.me/v2/bot/message/reply
+
+
+<?php
+$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+ // Loop through each event
+ foreach ($events['events'] as $event) {
+  // Reply only when message sent is in 'text' format
+  if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+   // Get text sent
+   $text = $event['message']['text'];
+   // Get replyToken
+   $replyToken = $event['replyToken'];
+
+   // Build message to reply back
+   $messages = [
+    'type' => 'text',
+    'text' => $text
+   ];
+
+   // Make a POST Request to Messaging API to reply to sender
+   $url = 'https://api.line.me/v2/bot/message/reply';
+   $data = [
+    'replyToken' => $replyToken,
+    'messages' => [$messages],
+   ];
+   $post = json_encode($data);
+   $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+   $ch = curl_init($url);
+   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   $result = curl_exec($ch);
+   curl_close($ch);
+
+   echo $result . "\r\n";
+  }
+ }
+}
+echo "OK";
