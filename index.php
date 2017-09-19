621951600986d4c1969e3bce1ac8d8762d80b21c
nuttakrantraiukhot <31967424+nuttakrantraiukhot@users.noreply.github.com>
2017-09-19 15:31:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <php?
 {
-  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "replyToken": "/jlPwu9B9zHK3uZu+NlIRAoTqhYISpzevh1qeAVyqNKDVpM5X7+GtW3aK1gdB6k96Fkd14LElyqGZ2y1Ln59AY2/F6+0jVtmsIyUFArhRmB7ui1WcYNAEDNYwslg4UvDVxz/fhKLSxZFOmLah1gaTAdB04t89/1O/w1cDnyilFU=",
   "type": "message",
   "timestamp": 1462629479859,
   "source": {
     "type": "user",
-    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+    "userId": "Uc86e42f9f17d087f2c6ca6fae436028c"
   },
   "message": {
     "id": "325708",
