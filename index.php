074ec72b92272fab163b4c8faab58d35b47343d4
nuttakrantraiukhot <31967424+nuttakrantraiukhot@users.noreply.github.com>
2017-09-19 20:57:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,2 @@
-{
-  "events": [
-      {
-        "replyToken": "/jlPwu9B9zHK3uZu+NlIRAoTqhYISpzevh1qeAVyqNKDVpM5X7+GtW3aK1gdB6k96Fkd14LElyqGZ2y1Ln59AY2/F6+0jVtmsIyUFArhRmB7ui1WcYNAEDNYwslg4UvDVxz/fhKLSxZFOmLah1gaTAdB04t89/1O/w1cDnyilFU=",
-        "type": "message",
-        "timestamp": 1462629479859,
-        "source": {
-             "type": "user",
-             "userId": "Uc86e42f9f17d087f2c6ca6fae436028c"
-         },
-         "message": {
-             "id": "325708",
-             "type": "text",
-             "text": "Hello, world"
-          }
-      }
-  ]
-}
+<?php
+echo "Hello LINE BOT";
